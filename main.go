d95b78fb44a51ed14f9bfe7c77c913f99fa6748d
2ono <clt3555@gmail.com>
2023-12-23 09:08:46 +0900

    basics methods

--- main.go
+++ main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World")
+	fmt.Println("Hello World.")
+
+	var whatToSay string = "Good Byecruel world"
+
+	var i int
+	i = 29
+
+	i = 30
+	whatToSay = "Good"
+	fmt.Println(whatToSay)
+	fmt.Println(i)
+
+	i = 8
+
+	fmt.Println("i is set to ", i)
+
+	wwhatWasSaid, theotehrThingThatwasSaid := saySomething()
+
+	fmt.Println("The Function returned", wwhatWasSaid, theotehrThingThatwasSaid)
+
+}
+
+func saySomething() (string, string) {
+	return "something", "else"
 }
