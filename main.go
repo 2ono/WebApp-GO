28de079f4304b34d9b9a862c5c64ff451a0da58f
2ono <clt3555@gmail.com>
2023-12-28 13:42:50 +0900

    loops and ranging over data

--- main.go
+++ main.go
@@ -4,20 +4,23 @@ import "log"
 
 func main() {
 
-	myVar := "tiger"
+	type User struct {
+		FirstName string
+		LastName  string
+		Email     string
+		Age       int
+	}
 
-	switch myVar {
-	case "cat":
-		log.Println("cat is set to cat")
-	case "dog":
-		log.Println("cat is set to dog")
-	case "fish":
-		log.Println("cat is set to fish")
-	case "tiger":
-		log.Println("cat is set to tiger")
-	default:
-		log.Println("everything is here")
+	var users []User
+	users = append(users, User{"John", "Smith", "Test@gmail.com", 23})
+	users = append(users, User{"mary", "Kim", "Test1@gmail.com", 20})
+	users = append(users, User{"King", "Harry", "Test2@gmail.com", 10})
+	users = append(users, User{"Quen", "Brow", "Test3@gmail.com", 50})
+	users = append(users, User{"Roy", "Red", "Test4@gmail.com", 42})
+	users = append(users, User{"Alex", "Jun", "Test5@gmail.com", 70})
 
+	for _, l := range users {
+		log.Println(l.FirstName, l.LastName, l.Email, l.Age)
 	}
 
 }
